2b1f0dbd5824390cef7996f83f924e4f86a73b12
gray <115596729+letlovewin@users.noreply.github.com>
2023-10-24 22:04:34 +0000

    Set up Firebase for this project, gave it a URL. To visit it go to brain-gaze.web.app

---
+++ public/scripts/main.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyC20zw-ac0BmCXlg8IfiwcdUdNqJKQsoSA",
+  authDomain: "brain-gaze.firebaseapp.com",
+  projectId: "brain-gaze",
+  storageBucket: "brain-gaze.appspot.com",
+  messagingSenderId: "59427742471",
+  appId: "1:59427742471:web:169793d2e414bb08545ba2",
+  measurementId: "G-PHW8RC7KXT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+document.getElementById("btn-ocular-track-start").addEventListener("click", function(){
+	webgazer.setGazeListener(function(data, elapsedTime) {
+		if (data == null) {
+			return;
+		}
+		var xprediction = data.x; //these x coordinates are relative to the viewport
+		var yprediction = data.y; //these y coordinates are relative to the viewport
+		console.log(elapsedTime); //elapsed time is based on time since begin was called
+	}).begin();
+	
+})
